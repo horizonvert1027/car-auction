3f1a5c319723c380fdd991da43ee317c778a4d6e
Ngo Le Nhat Minh <minh.nln@kiwi-universe.com>
2020-04-26 16:31:02 +0700

    Implement login form

--- application/models/Register_model.php
+++ application/models/Register_model.php
@@ -10,12 +10,12 @@ class Register_model extends CI_Model
 	function verify_email($key)
 	{
 		$this->db->where('verification_key', $key);
-		$this->db->where('is_email_verified', 'no');
+		$this->db->where('is_email_verified', false);
 		$query = $this->db->get('user');
 		if($query->num_rows() > 0)
 		{
 			$data = array(
-				'is_email_verified'  => 'yes'
+				'is_email_verified'  => true
 			);
 			$this->db->where('verification_key', $key);
 			$this->db->update('user', $data);
